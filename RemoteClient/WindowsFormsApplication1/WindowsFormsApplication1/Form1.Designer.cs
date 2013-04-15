d2e7abaa57395e6c80ae87d98209eae25aacfb33
a.grunin <a.grunin@itima.ru>
2013-04-15 09:51:40 +0400

    RemoteClient init

---
+++ RemoteClient/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отправить запрос";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 358);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 358);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "GPS y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "GPS x";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 26);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Сгенерировать массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(63, 36);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(369, 236);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Geo";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(164, 317);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(268, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(63, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 20);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Выбрать...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Файл";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(204, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Запрос";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 457);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Sensor client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RemoteClient/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            var boundary = "------------------------" + DateTime.Now.Ticks;
+            var newLine = Environment.NewLine;
+            var propFormat = "--" + boundary + newLine +
+                    "Content-Disposition: form-data; name=\"{0}\"" + newLine + newLine +
+                    "{1}" + newLine;
+            var fileHeaderFormat = "--" + boundary + newLine +
+                                    "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"" + newLine;
+
+            var req = (HttpWebRequest)HttpWebRequest.Create("http://yup.dev-forge.ru");
+            req.Method = WebRequestMethods.Http.Post;
+            req.ContentType = "multipart/form-data; boundary=" + boundary;
+            req.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.4) Gecko/20091016 Firefox/3.5.4 GTB6 (.NET CLR 3.5.30729)";
+            req.Timeout = 10000;
+
+
+            string array = textBox4.Text;
+            string x = textBox2.Text;
+            string y = textBox3.Text;
+
+            byte[] file = File.ReadAllBytes(textBox5.Text);
+            string encoded_file = Convert.ToBase64String(file);
+            string ext = System.IO.Path.GetExtension(openFileDialog1.FileName).ToLower();
+            if (ext == "jpg") ext = "jpeg";
+            string content_type = "Content-Type: image/"+ext;
+
+            string postData = "image=" + encoded_file + "&gps_x=" + x + "&gps_y=" + y + "&geo_data=" + array;
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            using (var reqStream = req.GetRequestStream()) 
+            {
+                var reqWriter = new StreamWriter(reqStream);
+                var tmp = string.Format(fileHeaderFormat, "image", System.IO.Path.GetFileName(textBox5.Text)) + 
+                    content_type + newLine + newLine + encoded_file + newLine + newLine;
+                reqWriter.Write(tmp);
+                tmp = string.Format(propFormat, "gps_x", x);
+                reqWriter.Write(tmp);
+                tmp = string.Format(propFormat, "gps_y", y);
+                reqWriter.Write(tmp);
+                tmp = string.Format(propFormat, "geo_data", array);
+                reqWriter.Write(tmp);
+                reqWriter.Write("--" + boundary + "--");
+                reqWriter.Flush();
+            }
+
+            var res = req.GetResponse();
+            using (var resStream = res.GetResponseStream())
+            {
+                var reader = new StreamReader(resStream);
+                var data = reader.ReadToEnd();
+                MessageBox.Show("Данные дошли, сервер ответил " + ((HttpWebResponse)res).StatusDescription,
+                    "Всё очень, очень хорошо.",
+                    MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox5.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void generate_array()
+        {
+            int Min = 0;
+            int Max = 32000;
+            int[] arr = new int[516]; 
+
+            Random randNum = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = randNum.Next(Min, Max);
+            }
+
+            textBox4.Text = "[" + String.Join(",", arr.Select(p => p.ToString()).ToArray()) + "]";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generate_array();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generate_array();
+        }
+    }
+}
